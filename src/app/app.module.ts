578c7c7170bc56e03f8e645d152c5ba1ed7154b1
Mariel Latella Barros <mariel.latella@gmail.com>
2020-10-27 10:05:49 +0100

    Lab 5: Template driven forms - create form component
    ng generate component contact-form // [myContactFormName]-form
    Generates form with its own testing section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { ContactComponent } from './contact/contact.component'
 import { AppComponent } from './app.component';
 import { NewsletterComponent } from './newsletter/newsletter.component';
 import { FormsModule } from '@angular/forms';
+import { ContactFormComponent } from './contact-form/contact-form.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ContactComponent,
     NewsletterComponent,
+    ContactFormComponent,
   ],
   imports: [
     BrowserModule,
