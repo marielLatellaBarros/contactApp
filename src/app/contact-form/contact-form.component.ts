780afd3b472b3e5e018edad1a3273db8bf1577ef
Mariel Latella Barros <mariel.latella@gmail.com>
2020-10-27 10:43:43 +0100

    Lab 5: add properties to form class to bind these ones with the form elements

--- src/app/contact-form/contact-form.component.ts
+++ src/app/contact-form/contact-form.component.ts
@@ -6,6 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./contact-form.component.css']
 })
 export class ContactFormComponent implements OnInit {
+  name: string;
+  email: string;
+  phone: string;
+  //https://stackoverflow.com/questions/41791933/in-typescript-what-is-the-type-of-image
+  // avatar: HTMLImageElement;
+  avatar: string;
+  isFavorite: boolean;
 
   constructor() { }
 
