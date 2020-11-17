a6d723e42e3148642270169cf74c3c63e92d654b
Mariel Latella Barros <mariel.latella@gmail.com>
2020-11-17 12:29:25 +0100

    Refactor contact component and model to support list of objects from Firebase
    Firebase doesn't support arrays, but lists of objects. Each object has a unique key.
    This key needs to be added to the structure of the code. => id
    
    NOTE: a required parameter cannot follow an optional parameter => if id is optional, put it at the end of the constructor parameters!
    
    Remove also the index given to the contact as this one has its own id now

--- src/app/models/contact.models.ts
+++ src/app/models/contact.models.ts
@@ -1,11 +1,13 @@
 export class Contact {
+    id: string;
     name: string;
     email: string;
     phone: string;
     isFavorite: boolean;
     avatar: string;
 
-    constructor (name: string, email: string, phone: string, isFavorite: boolean, avatar: 'assets/avatar.png',) {
+    constructor (name: string, email: string, phone: string, isFavorite: boolean, avatar: 'assets/avatar.png',id?: string) {
+        this.id = id;
         this.name = name;
         this.email = email;
         this.phone = phone;
