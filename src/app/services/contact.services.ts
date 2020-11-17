3f418135a8722807ff2862c85ae26ea8cf2348c6
Mariel Latella Barros <mariel.latella@gmail.com>
2020-11-17 12:43:27 +0100

    Add base url and implement getContactList method
    - getContactList makes an http call and converts the JSON format to contact objects. An Observable is returned with the data or an error. We can apply different operations to Observables (map, for instance). Working with Observables is similar to working with arrays.
    
    - http.get gets the data form the API.
    - data is converted to JSON.
    - JSON object is given to the method parseContactData via the map operator (for each object it will parse the data and return something)
    
    parseData
    - Firebase uses objects linked to unique keys.
    - Via Object.keys we get the key of the object.
    - Via the map operator we give each key to a function.
    - This function creates a new contact from the values that are attached to this key.
    - We return an array of Contacts (Contact[]) that we can then use in the application.

--- src/app/services/contact.services.ts
+++ src/app/services/contact.services.ts
@@ -4,14 +4,34 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 
+
+const BASEAPIURL: string = 'https://webexpert1718.firebaseio.com/contacts.json';
+
 @Injectable()
 export class ContactSerivce {
     contactList;
 
+
     constructor(private http: HttpClient) {}
 
-    getContactList(): Contact[] {
-        return this.contactList;
+    getContactList(): Observable<Contact[]> {
+        return this.http.get(BASEAPIURL).pipe(
+            map(this.parseContactData)
+        );
+
+    }
+    parseContactData(rawContacts: any): Contact[] {
+        return Object.keys(rawContacts).map(key => {
+            let contact = rawContacts[key];
+            return new Contact(
+                contact.name,
+                contact.email,
+                contact.phone,
+                contact.isFavorite,
+                contact.avatar,
+                key
+            );
+        });
     }
 
     addContact(contact: Contact) {
