4314f1de551efd38c8ecbc4805b07bce683449c2
Mariel Latella Barros <mariel.latella@gmail.com>
2020-11-17 10:58:57 +0100

    Create a contact service to get contacts, add and toggle Favourite property

---
+++ src/app/services/contact.services.ts
@@ -0,0 +1,23 @@
+import { Contact } from '../models/contact.models';
+
+export class ContactSerivce {
+    contactList = [
+        new Contact('jane doe', 'jane.doe@gmail.com', '01134543212', true, "assets/avatar.png"),
+        new Contact('john doe', 'john.doe@gmail.com', '01144323424', false, "assets/avatar.png"),
+        new Contact('Dries Swinnen', 'dries.swinnen@gmail.com', '01122233322', true, "assets/avatar.png"),
+    ];
+
+    constructor() {}
+
+    getContactList(): Contact[] {
+        return this.contactList;
+    }
+
+    addContact(contact: Contact) {
+        this.contactList.push(contact);
+    }
+
+    toggleFavorite(index: number): void {
+        this.contactList[index].isFavorite = !this.contactList[index].isFavorite;
+    }
+}
