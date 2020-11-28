0c52e8ddeceb02439fd89e76d42b74ae30227e5c
Mariel Latella Barros <mariel.latella@gmail.com>
2020-11-28 14:52:43 +0100

    Add main navigation and style and also to each contact of the list.
    Fix checkbox that was showing the details after clicking on it

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,35 +1,35 @@
-import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core"
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { Contact } from '../models/contact.model';
-import { ContactService } from "../services/contact.service";
+import { ContactService } from '../services/contact.service';
 
 @Component({
-    selector:'app-contact',
-    templateUrl: './contact.component.html',
-    styleUrls : ['./contact.component.css']
+  selector: 'app-contact',
+  templateUrl: './contact.component.html',
+  styleUrls: ['./contact.component.css'],
 })
-
 export class ContactComponent implements OnInit {
-    @Input() contact: Contact;
-    @Input() index: number;
-    @Output() onUpdate: EventEmitter<any> = new EventEmitter();
+  @Input() contact: Contact;
+  @Input() index: number;
+  @Output() onUpdate: EventEmitter<any> = new EventEmitter();
 
-    constructor(private contactService: ContactService){}
+  constructor(private contactService: ContactService) {}
 
-    ngOnInit(): void {
-        throw new Error('Method not implemented.');
-    }
-    name: string;
-    email: string;
-    phone: string;
-    isFavorite: boolean;
+  ngOnInit(): void {
+    throw new Error('Method not implemented.');
+  }
+  name: string;
+  email: string;
+  phone: string;
+  isFavorite: boolean;
 
-    onClick(): void {
-        console.log('Button clicked. Status van favorite is:' + this.isFavorite);
-    }
+  onClick(): void {
+    console.log('Button clicked. Status van favorite is:' + this.isFavorite);
+  }
 
-    toggleFavorite(id: string, isFavorite: boolean): void {
-        this.contactService.updateContact(id, {isFavorite: isFavorite}).subscribe(() => {
-            this.onUpdate.emit();
-        });
-    }
+  toggleFavorite(event: any, id: string, isFavorite: boolean): void {
+    event.stopPropagation();
+    this.contactService
+      .updateContact(id, { isFavorite: !isFavorite })
+      .subscribe(() => this.onUpdate.emit);
+  }
 }
