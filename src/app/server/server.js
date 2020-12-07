50a2245155e7c42ea0ba5d1dfa4d3f8d35a1986e
Mariel Latella Barros <mariel.latella@gmail.com>
2020-12-07 10:28:27 +0100

    Define main route "/"

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -1,5 +1,9 @@
 var express = require("express");
 var app = express();
 
+app.get("/", function (req, res) {
+  res.send("Hello - dit is express!");
+});
+
 app.listen(3000);
 console.log("Express server gestart op localhost:3000");
