265b60e34529a31616849c7081a2bcd4083eec61
Mariel Latella Barros <mariel.latella@gmail.com>
2020-12-07 10:30:15 +0100

    Return a json object in the response using res.json(json object)

--- src/app/server/server.js
+++ src/app/server/server.js
@@ -5,5 +5,14 @@ app.get("/", function (req, res) {
   res.send("Hello - dit is express!");
 });
 
+// 2. JSON retour zenden
+let persoon = {
+  voornaam: "Peter",
+  achternaam: "Kassenaar",
+};
+app.get("/json", function (req, res) {
+  res.json(persoon);
+});
+
 app.listen(3000);
 console.log("Express server gestart op localhost:3000");
